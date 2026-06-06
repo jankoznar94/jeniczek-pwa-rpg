04724b608d27aa09c41268321e7925302e49dc11
Jan Kožnár <jankoznar94@gmail.com>
2026-06-06 18:38:52 +0200

    twin icons: two opposite blue arrows, vertically offset

--- dist/game.js
+++ dist/game.js
@@ -615,7 +615,7 @@
     if (c.block > 0) icons.push('<span style="font-size:20px">🛡️</span>');
     if (c.wait > 0) icons.push('<span style="font-size:20px">⏳</span>');
     if (c.inverted > 0) icons.push(_arrowSvg('#5fa87a'));
-    if (c.twin > 0) icons.push(_arrowSvg('#5a8aaa'));
+    if (c.twin > 0) icons.push('<svg viewBox="0 -2 16 22" width="22" height="26"><path d="M8 1L13 8L10.5 8L10.5 15L5.5 15L5.5 8L3 8L8 1Z" fill="#5a8aaa" stroke="#5a8aaa" stroke-width="1.5" stroke-linejoin="round" stroke-linecap="round" transform="translate(0,-3.5)"/><path d="M8 15L3 8L5.5 8L5.5 1L10.5 1L10.5 8L13 8L8 15Z" fill="#5a8aaa" stroke="#5a8aaa" stroke-width="1.5" stroke-linejoin="round" stroke-linecap="round" transform="translate(0,3.5)"/></svg>');
     return icons;
   }
 
