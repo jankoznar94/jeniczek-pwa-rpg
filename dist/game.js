ceaa040e9a3e0022d398aa02206610fd7abe1c0b
Jan Kožnár <jankoznar94@gmail.com>
2026-06-12 15:17:49 +0200

    Fix: SVG dashoffset bonusCircum - zoneStartPx (správný směr offsetu)

--- dist/game.js
+++ dist/game.js
@@ -1212,7 +1212,7 @@
     const bonusCircle = document.querySelector('.bonus-zone-circle');
     if (bonusCircle) {
       bonusCircle.style.strokeDasharray = `${zoneWidthPx} ${bonusCircum - zoneWidthPx}`;
-      bonusCircle.style.strokeDashoffset = zoneStartPx;
+      bonusCircle.style.strokeDashoffset = bonusCircum - zoneStartPx;
     }
     mb._zoneWidthPx = zoneWidthPx;
     mb._zoneStartPx = zoneStartPx;
