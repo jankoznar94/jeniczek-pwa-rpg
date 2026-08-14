ed1823a2d901cbb037352627d1f782df673c9adf
CarandraugCZ <CarandraugCZ@users.noreply.github.com>
2026-08-14 15:40:50 +0000

    deploy: bb2ac9f5d5bc8c6474b9ae0f0f22e2edf9223926

---
+++ build/browserAll-Cfeo5nxe.js
@@ -0,0 +1,1 @@
+import{extensions as i,AccessibilitySystem as t,Container as e,accessibilityTarget as n,DOMPipe as a,EventSystem as d,FederatedContainer as m}from"./index-BKv-5WCy.js";import"./webworkerAll-B6V8E6m5.js";import"./index-9Trlx1tI.js";i.add(t);i.mixin(e,n);i.add(a);i.add(d);i.mixin(e,m);
