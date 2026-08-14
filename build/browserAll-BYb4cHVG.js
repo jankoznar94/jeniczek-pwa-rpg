320c73b0eccb9f2c053533f1ae9aec05024dc0ac
CarandraugCZ <CarandraugCZ@users.noreply.github.com>
2026-08-14 07:47:15 +0000

    deploy: d8e92e012fc97afcb0dcb1fa001296aff51ccff6

--- build/browserAll-BYb4cHVG.js
+++
@@ -1,1 +0,0 @@
-import{extensions as i,AccessibilitySystem as t,Container as e,accessibilityTarget as n,DOMPipe as a,EventSystem as d,FederatedContainer as m}from"./index-DyMIwF7h.js";import"./webworkerAll-DOV2_EgM.js";import"./index-Cq5us1xK.js";i.add(t);i.mixin(e,n);i.add(a);i.add(d);i.mixin(e,m);
