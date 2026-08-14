e864f8efac7f0b9247503133e2e373e45f250472
CarandraugCZ <CarandraugCZ@users.noreply.github.com>
2026-08-14 06:28:11 +0000

    deploy: 535ac58d6eb5169f1ce06afecabf98599fc9480c

--- build/browserAll-DluNdd1v.js
+++
@@ -1,1 +0,0 @@
-import{extensions as i,AccessibilitySystem as t,Container as e,accessibilityTarget as n,DOMPipe as a,EventSystem as d,FederatedContainer as m}from"./index-DCImUu6c.js";import"./webworkerAll-CSJdUrWL.js";import"./index-CmVucFEl.js";i.add(t);i.mixin(e,n);i.add(a);i.add(d);i.mixin(e,m);
