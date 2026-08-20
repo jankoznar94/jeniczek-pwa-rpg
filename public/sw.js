dd7d679c0f72f7e6d5fc57de35354d20672ab27e
Jan Kožnář <Jan.koznar.94@gmail.com>
2026-08-20 10:41:24 +0200

    One-hand zbrane: prirazeni 5 obrázku mečů a 5 obrázku seker (War Axe=hatchet, Military Pick=twin)

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CODE_CACHE = 'boss-slayer-code-v145';
+const CODE_CACHE = 'boss-slayer-code-v146';
 const ASSET_CACHE = 'boss-slayer-assets-v36';
 
 const CODE_FILES = ['/', '/index.html', '/style.css?v=132', '/manifest.json', '/item-reference.html'];
