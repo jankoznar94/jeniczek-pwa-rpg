aae677c371656fef58dbc95b07381ad984be7c5e
CarandraugCZ <CarandraugCZ@users.noreply.github.com>
2026-08-21 17:16:29 +0000

    deploy: ce4b7bcd8e4b25becf5d4b7abbe1692de14e9077

--- build/browserAll-VWBbNaMc.js
+++
@@ -1,1 +0,0 @@
-import{extensions as i,AccessibilitySystem as t,Container as e,accessibilityTarget as n,DOMPipe as a,EventSystem as d,FederatedContainer as m}from"./index-BWwGXFiF.js";import"./webworkerAll-gGyFyBdm.js";import"./index-Devk2ho7.js";i.add(t);i.mixin(e,n);i.add(a);i.add(d);i.mixin(e,m);
