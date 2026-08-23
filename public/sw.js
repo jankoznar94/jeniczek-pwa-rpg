2456302bd68cda1b9dca529fc16c9f14ba8c9f11
Jan Kožnář <Jan.koznar.94@gmail.com>
2026-08-23 19:04:27 +0200

    Zrušen horní border rozbaleného obsahu aktu (.map-loc-dot-scroll-wrap border-top)

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
-const CODE_CACHE = 'boss-slayer-code-v190';
+const CODE_CACHE = 'boss-slayer-code-v192';
 const ASSET_CACHE = 'boss-slayer-assets-v43';
 
-const CODE_FILES = ['/', '/index.html', '/style.css?v=152', '/manifest.json', '/item-reference.html'];
+const CODE_FILES = ['/', '/index.html', '/style.css?v=154', '/manifest.json', '/item-reference.html'];
 
 const ASSET_FILES = [
   '/icon-192.png', '/icon-512.png',
