4aa3c43631dc6ac41dd4389ea10797bd3a6dd954
CarandraugCZ <CarandraugCZ@users.noreply.github.com>
2026-08-23 12:10:18 +0000

    deploy: 2ea29037094a4427d0881de2c04c9ac44a31b103

---
+++ build/browserAll-B2qM4FKC.js
@@ -0,0 +1,1 @@
+import{extensions as i,AccessibilitySystem as t,Container as e,accessibilityTarget as n,DOMPipe as a,EventSystem as d,FederatedContainer as m}from"./index-Cu2IaK46.js";import"./webworkerAll-C4ex7z7j.js";import"./index-Clrf-i6U.js";i.add(t);i.mixin(e,n);i.add(a);i.add(d);i.mixin(e,m);
