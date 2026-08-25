843302f45f090b6b960d719ac045550e45ddd529
CarandraugCZ <CarandraugCZ@users.noreply.github.com>
2026-08-25 10:32:14 +0000

    deploy: 4fa226d28a8d62f9798190251c65ce803416b6d7

--- build/browserAll-BQYYeRK1.js
+++
@@ -1,1 +0,0 @@
-import{extensions as i,AccessibilitySystem as t,Container as e,accessibilityTarget as n,DOMPipe as a,EventSystem as d,FederatedContainer as m}from"./index-DdT_SERX.js";import"./webworkerAll-CzUU4Wpw.js";import"./index-BKKdQ0j7.js";i.add(t);i.mixin(e,n);i.add(a);i.add(d);i.mixin(e,m);
