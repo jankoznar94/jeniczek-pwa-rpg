594a579df9dfa8109f094d37ceeaf45f44415ccf
CarandraugCZ <CarandraugCZ@users.noreply.github.com>
2026-08-25 18:11:57 +0000

    deploy: 0af78db2ddbf5d15191e9580834f30f7ef4f244f

--- build/browserAll--wSKJ2Au.js
+++
@@ -1,1 +0,0 @@
-import{extensions as i,AccessibilitySystem as t,Container as e,accessibilityTarget as n,DOMPipe as a,EventSystem as d,FederatedContainer as m}from"./index-DDnI4ky3.js";import"./webworkerAll-D0EUoyz4.js";import"./index-Ct_ppLDe.js";i.add(t);i.mixin(e,n);i.add(a);i.add(d);i.mixin(e,m);
--- build/webworkerAll-D0EUoyz4.js
+++
@@ -1,1 +0,0 @@
-import{extensions as e,FilterSystem as t,CanvasFilterSystem as i,FilterPipe as r}from"./index-DDnI4ky3.js";import"./index-Ct_ppLDe.js";e.add(t,i);e.add(r);
