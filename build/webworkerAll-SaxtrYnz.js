3c83a6b466e67f4350778ef3a4a8299918353a65
CarandraugCZ <CarandraugCZ@users.noreply.github.com>
2026-08-26 07:23:43 +0000

    deploy: ce5b86ce528bfe02598bc2c666d136d795b94c05

--- build/webworkerAll-SaxtrYnz.js
+++
@@ -1,1 +0,0 @@
-import{extensions as e,FilterSystem as t,CanvasFilterSystem as i,FilterPipe as r}from"./index-CJjaqs9-.js";import"./index-CRXW-x5t.js";e.add(t,i);e.add(r);
--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CODE_CACHE = 'boss-slayer-code-v206';
+const CODE_CACHE = 'boss-slayer-code-v207';
 const ASSET_CACHE = 'boss-slayer-assets-v51';
 
-const CODE_FILES = ['/', '/index.html', '/style.css?v=163', '/manifest.json', '/item-reference.html'];
+const CODE_FILES = ['/', '/index.html', '/style.css?v=164', '/manifest.json', '/item-reference.html'];
 
 const ASSET_FILES = [
   '/icon-192.png', '/icon-512.png',
