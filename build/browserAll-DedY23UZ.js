5e27b9a40f4f4e941b7b9458ad42e9c4ee2b65d1
CarandraugCZ <CarandraugCZ@users.noreply.github.com>
2026-08-30 16:48:45 +0000

    deploy: e57ec16483e33248abd1e4a4a3a58a14c61c9c05

---
+++ build/browserAll-DedY23UZ.js
@@ -0,0 +1,1 @@
+import{extensions as i,AccessibilitySystem as t,Container as e,accessibilityTarget as n,DOMPipe as a,EventSystem as d,FederatedContainer as m}from"./index-MRxoR5G4.js";import"./webworkerAll-C66rJPQS.js";import"./index-3HZP-mt1.js";i.add(t);i.mixin(e,n);i.add(a);i.add(d);i.mixin(e,m);
