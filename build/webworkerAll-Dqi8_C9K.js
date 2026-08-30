82f29bb450c09e60f1424667664639fdf63958e7
CarandraugCZ <CarandraugCZ@users.noreply.github.com>
2026-08-30 21:18:01 +0000

    deploy: e23d2655c6c5f23134d3d8d939bc2140d7616e2f

---
+++ build/webworkerAll-Dqi8_C9K.js
@@ -0,0 +1,1 @@
+import{extensions as e,FilterSystem as t,CanvasFilterSystem as i,FilterPipe as r}from"./index-CiwNa13M.js";import"./index-C6XsQB2k.js";e.add(t,i);e.add(r);
--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CODE_CACHE = 'boss-slayer-code-v227';
+const CODE_CACHE = 'boss-slayer-code-v228';
 const ASSET_CACHE = 'boss-slayer-assets-v54';
 
-const CODE_FILES = ['/', '/index.html', '/style.css?v=172', '/manifest.json', '/item-reference.html'];
+const CODE_FILES = ['/', '/index.html', '/style.css?v=173', '/manifest.json', '/item-reference.html'];
 
 const ASSET_FILES = [
   '/icon-192.png', '/icon-512.png',
