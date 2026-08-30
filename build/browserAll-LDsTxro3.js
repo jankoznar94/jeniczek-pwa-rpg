ed2c561dc7b4102be2f2595000fc3ba221810ce0
CarandraugCZ <CarandraugCZ@users.noreply.github.com>
2026-08-30 18:22:51 +0000

    deploy: e404c9a4882f672727821ac27f39882d86732cbc

--- build/browserAll-LDsTxro3.js
+++
@@ -1,1 +0,0 @@
-import{extensions as i,AccessibilitySystem as t,Container as e,accessibilityTarget as n,DOMPipe as a,EventSystem as d,FederatedContainer as m}from"./index-Dmnvm88E.js";import"./webworkerAll-Cxhy5ZhF.js";import"./index-01O5wli_.js";i.add(t);i.mixin(e,n);i.add(a);i.add(d);i.mixin(e,m);
