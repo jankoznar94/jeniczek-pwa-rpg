91458c8c1ce8bbc1265489a2c0ba8bee2e989529
CarandraugCZ <CarandraugCZ@users.noreply.github.com>
2026-08-31 18:06:25 +0000

    deploy: f24f8a6113d927118037f19161815b97ed937aff

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CODE_CACHE = 'boss-slayer-code-v237';
+const CODE_CACHE = 'boss-slayer-code-v238';
 const ASSET_CACHE = 'boss-slayer-assets-v55';
 
-const CODE_FILES = ['/', '/index.html', '/style.css?v=173', '/manifest.json', '/item-reference.html'];
+const CODE_FILES = ['/', '/index.html', '/style.css?v=174', '/manifest.json', '/item-reference.html'];
 
 const ASSET_FILES = [
   '/icon-192.png', '/icon-512.png',
